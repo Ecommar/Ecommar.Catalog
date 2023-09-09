d6b994b4fd2723bd7bdad40df770859aabae2a03
Nabil458 <75916357+Nabil458@users.noreply.github.com>
2023-09-09 17:02:43 +0200

    connecting to the database (#7)

--- src/Ecommar.Catalog.Repositories/CatalogRepository.cs
+++ src/Ecommar.Catalog.Repositories/CatalogRepository.cs
@@ -1,10 +1,21 @@
 ﻿using Ecommar.Catalog.Models.DTOs;
 using Ecommar.Catalog.Repositories.Interfaces;
+using System;
+using Ecommar.Catalog.Infra;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Ecommar.Catalog.Repositories;
 
 public class CatalogRepository : ICatalogRepository
 {
+    private readonly DatabaseConfiguration _databaseConfiguration;
+
+
+    public CatalogRepository (DatabaseConfiguration databaseConfiguration)
+    {
+        _databaseConfiguration = databaseConfiguration;
+    }
     public Task<List<ProductDto>> GetAllProducts()
     {
         // In a real-world scenario, you'd fetch this from a database.
@@ -53,7 +64,30 @@ public class CatalogRepository : ICatalogRepository
                 }
             }
         };
+        string? connectionString = _databaseConfiguration.GetMongoDBConnectionString();
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("test");
 
+        // Specify the name of the collection (table)
+        string collectionName = "your-collection-name";
+
+        // Create a new collection (table) in MongoDB if it doesn't exist
+        var collection = database.GetCollection<BsonDocument>(collectionName);
+        collection.Indexes.CreateOne(new CreateIndexModel<BsonDocument>(Builders<BsonDocument>.IndexKeys.Ascending("_id")));
+
+        // Attempt to retrieve the collection's statistics to check if it exists
+        var collectionStats = database.RunCommand<BsonDocument>(new BsonDocument { { "collStats", collectionName } });
+
+        bool isCollectionCreated = collectionStats.Contains("ok");
+
+        if (isCollectionCreated)
+        {
+            var res = true; // Connection successful
+        }
+        else
+        {
+            var res = false; // Connection failed
+        }
         return Task.FromResult(products);
     }
 
